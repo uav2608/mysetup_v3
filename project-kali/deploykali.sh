24c9b20c857b72c2cb2bdd1b417a52de92a9dd58
uav2608 <uav2608@gmail.com>
2019-05-11 13:09:01 -0400

    11052019_1

--- project-kali/deploykali.sh
+++ project-kali/deploykali.sh
@@ -1,30 +1,28 @@
 #! /usr/bin/env bash
 
-VMNAME="kali"
+VIRT_TYPE="kvm"
+VMNAME="kali2"
 RAM="2048"
-#CPU="2"
-DISK_PATH="/var/lib/libvirt/images/Kali.qcow2"
-DISK_FORMAT="qcow2"
-DISK_SIZE="8"
+CPU="2"
 OS_TYPE="linux"
 OS_VARIANT="debianwheezy"
 LOCATION="/home/ansibleuser1/ISO/kali-linux-xfce-2019.1a-amd64.iso"
+DISK_PATH="/var/lib/libvirt/images/Kali.qcow2"
+DISK_FORMAT="qcow2"
+DISK_SIZE="8"
 NETWORK_BRIDGE="virbr0"
 
 virt-install \
+--virt-type=$VIRT_TYPE \
 --name $VMNAME \
---disk path=$DISK_PATH,size=$DISK_SIZE \
---ram=2048 \
---os-type $OS_TYPE \
---os-variant $OS_VARIANT \
+--ram $RAM \
+--vcpus=$CPU \
+--os-type=$OS_TYPE \
+--os-variant=$OS_VARIANT \
+--hvm \
+--cdrom $LOCATION \
+--disk path=$DISK_PATH,size=$DISK_SIZE,format=qcow2 \
 --network bridge=$NETWORK_BRIDGE \
---graphics none \
---cdrom $LOCATION 
+--graphics none 
+ 
 
-
-#--console pty,target_type=serial \
-#virt-install -n Kali -r 2048 /
-#--disk path=/var/lib/libvirt/images/Kali.qcow2,bus=virtio,size=50,format=qcow2 /
-#-c /var/lib/libvirt/images/iso/kali-linux-1.0.9a-amd64.iso /
-#--network network=default,model=virtio --connect=qemu:///system --vnc /
-#--noautoconsole --hvm --video=vmvga --os-type=linux --os-variant=debianwheezy
